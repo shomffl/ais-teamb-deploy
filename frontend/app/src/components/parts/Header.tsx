044028af51fa2054019363a7b68f739987920bcf
saki000 <hangchengtengqi@gmail.com>
2022-10-09 22:09:31 +0900

    react-iconsの追加

--- frontend/app/src/components/parts/Header.tsx
+++ frontend/app/src/components/parts/Header.tsx
@@ -1,30 +1,34 @@
+import { AiFillCaretDown } from "react-icons/ai";
 import image from "../../logo192.png";
 import NewButton from "./NewButton";
 
 const Header = () => {
   return (
-    <div className="h-20 w-screen grid grid-cols-2 content-center border-b gap-y-0">
-      <div className="justify-self-start ml-6 flex content-center">
-        <p className="pt-6 font-bold text-2xl text-navy2">AppName</p>
+    <div className="h-20 w-screen grid grid-cols-2 content-center border-b">
+      <div className="justify-self-start ml-6 flex items-center">
+        <p className="font-bold text-2xl text-navy2">AppName</p>
         <div className=" ml-16 flex space-x-6">
-          <div className="pt-7 hover:border-b hover:text-blue2 border-blue2 text-gray2">
+          <div className="py-7 hover:border-b hover:text-blue2 border-blue2 text-gray2 cursor-pointer">
             <p>My Events</p>
           </div>
-          <div className="pt-7 hover:border-b border-blue2 text-gray2 hover:text-blue2">
+          <div className="py-7 hover:border-b border-blue2 text-gray2 hover:text-blue2 cursor-pointer">
             <p>All Events</p>
           </div>
         </div>
       </div>
-      <div className="justify-self-end mr-6">
-        <div className="flex space-x-4 ">
-          <div className="py-6">
-            <NewButton />
-          </div>
+      <div className="justify-self-end mr-6 items-center flex space-x-4">
+        <div className="">
+          <NewButton />
+        </div>
 
-          <div className="py-6">
-            <img className="h-8 w-8 rounded-full " src={image} alt="" />
+        <div className="">
+          <img className="h-8 w-8 rounded-full " src={image} alt="" />
+        </div>
+        <div className="inline-flex items-center space-x-2 cursor-pointer">
+          <p>Names</p>
+          <div className="pt-1">
+            <AiFillCaretDown />
           </div>
-          <p className="py-7">Names</p>
         </div>
       </div>
     </div>
--- frontend/app/src/components/parts/NewButton.tsx
+++ frontend/app/src/components/parts/NewButton.tsx
@@ -1,7 +1,12 @@
+import { AiOutlinePlus } from "react-icons/ai";
+
 const NewButton = () => {
   return (
     <>
-      <button className="bg-navy2 text-white2 px-2 py-1 rounded-lg">New</button>
+      <button className="bg-navy2 text-white2 px-2 py-1 rounded-lg inline-flex items-center space-x-1">
+        <span>New</span>
+        <AiOutlinePlus />
+      </button>
     </>
   );
 };
