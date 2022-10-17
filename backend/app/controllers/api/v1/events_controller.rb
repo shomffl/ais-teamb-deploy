1c5e4dac31747d8b6c6317fcfaf36dd574db4b89
hibik17 <hbtatsu@gmail.com>
2022-10-18 01:06:08 +0900

    urlをapi/v1に修正

---
+++ backend/app/controllers/api/v1/events_controller.rb
@@ -0,0 +1,13 @@
+class EventsController < ApplicationController
+  def index
+    # indexでは全てのイベントを返却したい
+    @events = Event.all
+    render :json => @events
+  end
+
+  def edit
+  end
+
+  def show
+  end
+end
