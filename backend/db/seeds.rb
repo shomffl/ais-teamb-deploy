4b04d9bae65d61fb35fea8bc214d69c4e450d082
hibik17 <hbtatsu@gmail.com>
2022-10-18 00:57:10 +0900

    seed ファイルの作成

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# seedsのフォルダで作成したファイルをimport
+require './db/seeds/eventData'
+require './db/seeds/schoolData'
---
+++ backend/db/seeds/schoolData.rb
@@ -0,0 +1,7 @@
+puts 'school ダミーデータの作成開始'
+
+10.times do
+  School.create(
+    name:Faker::Name.name
+  )
+end
