df0302d1c7e3b59758848a4f9e14fd4cd3f5aac4
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-10-27 14:08:32 +0900

    tweak edit route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :events
       get 'api/v1/events/new' => 'events#new'
-      get 'api/v1/events/edit' => 'events#edit'
+      get 'api/v1/events/:id/edit' => 'events#edit'
     end
   end
 end
