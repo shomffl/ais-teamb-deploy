b1975f6f111a9d00042cf50b14e24d8cd6fcb407
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-10-27 13:55:50 +0900

    add new and edit routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :events
+      get 'api/v1/events/new' => 'events#new'
+      get 'api/v1/events/edit' => 'events#edit'
     end
   end
 end
