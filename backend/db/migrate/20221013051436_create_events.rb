d5a59b119b4d7047fbb88a674905b8d6aa86e208
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-10-31 18:22:26 +0900

    tweak image_path column

--- backend/db/migrate/20221013051436_create_events.rb
+++ backend/db/migrate/20221013051436_create_events.rb
@@ -6,7 +6,7 @@ class CreateEvents < ActiveRecord::Migration[6.1]
       t.integer :company_id
       t.string :name
       t.text :detail
-      t.text :image_path
+      t.text :image_path, limit: 4294967295
 
       t.timestamps
     end
