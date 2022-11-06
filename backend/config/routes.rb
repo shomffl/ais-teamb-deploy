b7154eabce8167ac2ce66fb245a3c5cb5f309142
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-11-06 13:40:53 +0900

    make registrations and sessions controller

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -7,6 +7,14 @@ Rails.application.routes.draw do
       resources :events
       get 'api/v1/events/new' => 'events#new'
       get 'api/v1/events/:id/edit' => 'events#edit'
+
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+
+      namespace :auth do
+        resources :sessions, only: %i[index]
+      end
     end
   end
 end
