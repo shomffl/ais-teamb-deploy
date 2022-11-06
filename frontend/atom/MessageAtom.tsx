16f0a7ae9b7692715586e1ebf74d7eb167fa0799
saki000 <hangchengtengqi@gmail.com>
2022-11-06 14:20:39 +0900

    recoilの導入とmessageのスタイル実装

---
+++ frontend/atom/MessageAtom.tsx
@@ -0,0 +1,9 @@
+import { atom } from "recoil";
+
+export const messageState = atom({
+  key: "message",
+  default: {
+    open: false,
+    text: "",
+  },
+});
--- frontend/components/organisms/Message.tsx
+++ frontend/components/organisms/Message.tsx
@@ -1,11 +1,22 @@
 import { AiOutlineClose } from "react-icons/ai";
+import { useRecoilState } from "recoil";
+import { messageState } from "../../atom/MessageAtom";
 
 const Message = () => {
+  const [message, setMessage] = useRecoilState(messageState);
   return (
-    <div className="fixed bottom-8 right-8 border-2 bg-white border-gray2 w-56 h-14 rounded grid grid-cols-6 place-content-center place-items-center">
-      <div className="col-span-3 col-start-2"></div>
+    <div
+      className={`fixed bottom-8 right-8 border-2 bg-white border-gray2 w-56 h-14 rounded grid grid-cols-6 place-content-center place-items-center ${
+        message.open || "hidden"
+      }`}
+    >
+      <div className="col-span-3 col-start-2">{message.text}</div>
       <div className="col-end-7">
-        <AiOutlineClose />
+        <AiOutlineClose
+          onClick={() => {
+            setMessage({ ...message, open: false });
+          }}
+        />
       </div>
     </div>
   );
