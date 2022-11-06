92ea692e57cc26da7ce0830972590523f25b4ac6
agukk <a8120056@gsuite.si.aoyama.ac.jp>
2022-11-06 12:45:09 +0900

    install device and device_token_auth

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
       resources :events
