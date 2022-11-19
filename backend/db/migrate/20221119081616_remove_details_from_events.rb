00754a27b599dc3b2c8ac56303ac94e064028036
Kenta Kato <01keny01@gmail.com>
2022-11-19 21:02:02 +0900

    delete unnecessary migrate file and add column

--- backend/db/migrate/20221119081616_remove_details_from_events.rb
+++
@@ -1,6 +0,0 @@
-class RemoveDetailsFromEvents < ActiveRecord::Migration[6.1]
-  def change
-    remove_column :events, :image_path, :string
-    remove_column :events, :image, :string
-  end
-end
--- backend/db/migrate/20221119082155_add_details_to_events.rb
+++
@@ -1,6 +0,0 @@
-class AddDetailsToEvents < ActiveRecord::Migration[6.1]
-  def change
-    add_column :events, :image, :string, after: :detail
-    add_column :events, :image_path, :string, after: :image
-  end
-end
