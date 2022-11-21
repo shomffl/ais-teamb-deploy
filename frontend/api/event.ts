5949660588e697b4aee32d331eca05080893a6eb
saki000 <hangchengtengqi@gmail.com>
2022-11-21 17:19:09 +0900

    画像の表示をnext/Imageからimgに変更

--- frontend/api/event.ts
+++ frontend/api/event.ts
@@ -8,23 +8,34 @@ export const getMyEvent = () => {
   const { data, error, mutate } = useSWR(`${URL}/events`, fetcher);
   return { data, isLoading: !error && !data, error, mutate };
 };
-export const createEvent = async (data: any, setMessage: any) => {
+export const getEvent = (id: number) => {
+  const { data, error, mutate } = useSWR(`${URL}/events/${id}`, fetcher);
+  return { data, isLoading: !error && !data, error, mutate };
+};
+export const createEvent = async (data: any, setMessage: any, router: any) => {
   await axios
     .post(`${URL}/events`, data)
     .then((res) => {
       console.log(res);
       setMessage({ open: true, text: "Eventを追加しました" });
+      router.push("/company");
     })
     .catch((error) => {
       console.log(error);
     });
 };
-export const updateEvent = async (id: number, data: any, setMessage: any) => {
+export const updateEvent = async (
+  id: number,
+  data: any,
+  setMessage: any,
+  router: any
+) => {
   await axios
     .patch(`${URL}/events/${id}`, data)
     .then((res) => {
       console.log(res);
       setMessage({ open: true, text: "Eventを編集しました" });
+      router.push("/company");
     })
     .catch((err) => {
       console.log(err);
